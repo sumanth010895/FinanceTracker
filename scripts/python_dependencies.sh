d1b2967ca7182051397934a09730c1c3a59b1e4f
sumanth010895 <x22244263@student.ncirl.ie>
2023-11-28 00:26:56 +0500

    up

--- scripts/python_dependencies.sh
+++ scripts/python_dependencies.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+sudo chown -R ubuntu:ubuntu ~/financetracker
 virtualenv /home/ubuntu/financetracker/venv
 source /home/ubuntu/financetracker/venv/bin/activate
 pip install -r /home/ubuntu/financetracker/requirements.txt
